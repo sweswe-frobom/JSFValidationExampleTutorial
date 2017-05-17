32e6234d15c9d16043e95876d371bf5467b8fc43
sweswe-frobom <khine.swe.swe.aung@frobom.com>
2017-05-17 12:00:55 +0630

    first commit

---
+++ src/com/journaldev/jsf/bean/MobileValidator.java
@@ -0,0 +1,45 @@
+package com.journaldev.jsf.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@ManagedBean
+@SessionScoped
+@FacesValidator("mobileValidator")
+public class MobileValidator implements Validator {
+
+	private String mno;
+
+	public String getMno() {
+		return mno;
+	}
+
+	public void setMno(String mno) {
+		this.mno = mno;
+	}
+
+	int maximumlength = 6;
+
+	public MobileValidator() {
+	}
+
+	@Override
+	public void validate(FacesContext arg0, UIComponent arg1, Object arg2) throws ValidatorException {
+		// TODO Auto-generated method stub
+		String model = (String) arg2;
+
+		if (model.length() > 6) {
+			FacesMessage msg = new FacesMessage(" Maximum Length of 6 is exceeded.Please enter values within range");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+
+			throw new ValidatorException(msg);
+		}
+
+	}
+}
